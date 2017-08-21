d2e9d656f1be3852d7b8ce5d033ba82e939c3721
Eric Tiedeken <eric.tiedeken@encore.com>
2017-08-21 08:24:20 -0500

    Page 2 complete

--- whats-great/content.php
+++ whats-great/content.php
@@ -9,7 +9,7 @@
     <div id="Great" class="blockWrap">
         <div class="blockContent">
             <div class="blockSection">
-                <img src="/images/Great.png" width="461" height="275" alt="See What's Great!" />
+                <img src="/images/Great.png" width="509" height="301" alt="See What's Great!" />
             </div>
             <div class="blockSection">
                 <h1>See What's Great!</h1>
@@ -17,12 +17,12 @@
                     <li><a href="#Ready">Over 4,800 Ready-Made Templates</a></li>
                     <li><a href="#Commercial">Over 480 Royalty-Free, Commercial-Use Templates</a></li>
                     <li><a href="#Images">Over 165,000 Images and Photographs</a></li>
-                    <li><a href="#Avery"><b>Avery</b> Templates</a></li>
+                    <li><a href="#Avery"><b>Avery®</b> Templates</a></li>
                     <li><a href="#TextTool">Extraordinary Text Tools and Effects</a></li>
                     <li><a href="#Calendar">Calendar Tool</a></li>
                     <li><a href="#Design">Easy-To-Use Design Tools</a></li>
                     <li><a href="#OneClick">One-Click Photo Editing</a></li>
-                    <li><a href="#Scrapbook">Inspiring Scrapbook Design</a></li>
+                    <li><a href="#Scrapbook">Inspiring Scrapbook and Photo Book Designs</a></li>
                 </ul>
             </div>
         </div>
@@ -35,7 +35,7 @@
                 <p>PrintMaster Platinum includes easily searchable ready-made templates by professional designers to make your project quickly and easily.</p>
             </div>
             <div class="blockSection">
-                <img src="/images/Ready.png" width="700" height="272" class="pullright" alt="Over 4,700 Ready-Made Templates" />
+                <img src="/images/Ready.png" width="610" height="272" alt="Over 4,800 Ready-Made Templates" />
             </div>
         </div>
     </div>
@@ -43,7 +43,7 @@
     <div id="Commercial" class="blockWrap">
         <div class="blockContent">
             <div class="blockSection">
-                <img src="/images/Commercial.png" width="442" height="250" alt="Over 400 Royalty-Free Commercial-Use Templates" />
+                <img src="/images/Commercial.png" width="509" height="267" class="pullright" alt="Over 480 Royalty-Free Commercial-Use Templates" />
             </div>
             <div class="blockSection">
                 <h1>Over <span class="number">480</span> Royalty-Free Commercial-Use Templates</h1>
@@ -59,19 +59,49 @@
                 <p>Includes 165,000 High Quality Images royalty free images for personal and commercial use.</p>
             </div>
             <div class="blockSection">
-                <img src="/images/Images.png" width="672" height="280" class="pullright" alt="Over 165,000 Images and Photographs" />
+                <img src="/images/Images.png" width="567" height="355" class="pullright" alt="Over 165,000 Images and Photographs" />
             </div>
         </div>
     </div>
 
     <div id="Avery" class="blockWrap">
         <div class="blockContent">
+            <h6><img src="/images/Avery.png" width="353" height="70" alt="Avery® Templates" /> PrintMaster Platinum v8 includes even MORE Avery® templates</h6>
             <div class="blockSection">
-                <img src="/images/Avery.png" width="479" height="229" alt="Avery® Templates" />
+                <div class="col25">
+                    <img src="/images/AveryGreetingCards.png" width="81" height="83" alt="Greeting Cards" />
+                    <p>Greeting Cards</p>
+                </div>
+                <div class="col25">
+                    <img src="/images/AveryRecipeCards.png" width="94" height="65" alt="Recipe Cards" />
+                    <p>Recipe Cards</p>
+                </div>
+                <div class="col25">
+                    <img src="/images/AveryStickers.png" width="83" height="84" alt="Stickers" />
+                    <p>Stickers</p>
+                </div>
+                <div class="col25">
+                    <img src="/images/AveryBusinessCards.png" width="102" height="69" alt="Business Cards" />
+                    <p>Business Cards</p>
+                </div>
             </div>
             <div class="blockSection">
-                <h1>Avery<sup>&reg;</sup> Templates</h1>
-                <p>Easily create custom CD and DVD decals, mailing labels, stickers and more!</p>
+                <div class="col25">
+                    <img src="/images/AveryDiscLabels.png" width="78" height="78" alt="Disc Labels" />
+                    <p>Disc Labels</p>
+                </div>
+                <div class="col25">
+                    <img src="/images/AveryTags.png" width="69" height="72" alt="Tags" />
+                    <p>Tags</p>
+                </div>
+                <div class="col25">
+                    <img src="/images/AveryNameBadges.png" width="87" height="73" alt="Name Badges" />
+                    <p>Name Badges</p>
+                </div>
+                <div class="col25">
+                    <img src="/images/AveryShippingLabels.png" width="90" height="82" alt="Shipping Labels" />
+                    <p>Shipping Labels</p>
+                </div>
             </div>
         </div>
     </div>
@@ -83,7 +113,7 @@
                 <p>Text and photos can be used to fill a shape and position text to follow a line.</p>
             </div>
             <div class="blockSection">
-                <img src="/images/TextTool.png" width="591" height="279" class="pullright" alt="Extraordinary Text Tools and Effects" />
+                <img src="/images/TextTool.png" width="512" height="258" alt="Extraordinary Text Tools and Effects" />
             </div>
         </div>
     </div>
@@ -91,7 +121,7 @@
     <div id="Calendar" class="blockWrap">
         <div class="blockContent">
             <div class="blockSection">
-                <img src="/images/Calendar.png" width="698" height="302" alt="Calendar Tool" />
+                <img src="/images/Calendar.png" width="637" height="324" alt="Calendar Tool" />
             </div>
             <div class="blockSection">
                 <h1>Calendar Tool</h1>
@@ -108,7 +138,7 @@
                     <p>Drag-and-drop photos, text and design elements with incredible ease!</p>
                 </div>
                 <div class="col45">
-                    <img src="/images/Design.png" width="477" height="322" alt="Easy-to-Use Design Tools" />
+                    <img src="/images/Design.png" width="477" height="323" alt="Easy-to-Use Design Tools" />
                 </div>
             </div>
             <div class="blockSection">
@@ -118,6 +148,7 @@
                     <li><span>Drag and Drop Images</span></li>
                     <li><span>Slider Tools</span></li>
                     <li><span>Thumbnail Preview</span></li>
+                    <li><span>And More!</span></li>
                 </ul>
             </div>
         </div>
@@ -161,11 +192,11 @@
     <div id="Scrapbook" class="blockWrap">
         <div class="blockContent">
             <div class="blockSection">
-                <h1>Inspiring Scrapbook Designs</h1>
+                <h1>Inspiring Scrapbook and Photobook Designs</h1>
                 <p>Drop in borders, embellishments, backgrounds and more!</p>
             </div>
             <div class="blockSection">
-                <img src="/images/Scrapbook.png" width="659" height="342" class="pullright" alt="Inspiring Scrapbook Designs" />
+                <img src="/images/Scrapbook.png" width="661" height="355" class="pullright" alt="Inspiring Scrapbook Designs" />
             </div>
         </div>
     </div>
